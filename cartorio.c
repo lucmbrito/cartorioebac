e955179b89574918a7e20f61586ae6df185377c5
lucmbrito <146759069+lucmbrito@users.noreply.github.com>
2023-11-24 13:01:42 -0300

    login e senha atualizado

--- cartorio.c
+++ cartorio.c
@@ -111,14 +111,18 @@ int main()
 	int opcao=0; //Definindo variaveis
 	int laco=1;
 	char senhadigitada[10]="a";
+	int comparacao;
 	
 	printf("### Cartório do EBAC ###\n\n");
 	printf("Login de administrador!\n\n Digite a sua senha: ");
 	scanf("%s",senhadigitada);
 	
-	if(senhadigitada[10] == "admin")
+	comparacao = strcmp(senhadigitada, "admin");
+	
+	if(comparacao == 0)
 	
 	{
+		system("cls");
 		for(laco=1;laco=1;)
 		{
 	  
